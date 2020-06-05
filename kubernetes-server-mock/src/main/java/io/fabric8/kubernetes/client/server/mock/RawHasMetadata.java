05dc9e7427f05a4052dd97b10530f9db92197423
Bryan Beaudreault <bbeaudreault@hubspot.com>
2020-06-05 17:46:08 -0400

    add fallback for supporting raw customResource interactions

---
+++ kubernetes-server-mock/src/main/java/io/fabric8/kubernetes/client/server/mock/RawHasMetadata.java
@@ -0,0 +1,63 @@
+package io.fabric8.kubernetes.client.server.mock;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.fabric8.kubernetes.api.model.HasMetadata;
+import io.fabric8.kubernetes.api.model.ObjectMeta;
+
+public class RawHasMetadata implements HasMetadata {
+
+  private String apiVersion;
+  private String kind;
+  private ObjectMeta metadata;
+
+  /**
+   * No args constructor for use in serialization
+   *
+   */
+  public RawHasMetadata() {
+  }
+
+  public RawHasMetadata(String apiVersion, String kind, ObjectMeta metadata) {
+    this.apiVersion = apiVersion;
+    this.kind = kind;
+    this.metadata = metadata;
+  }
+
+
+  @Override
+  @JsonProperty("apiVersion")
+  public String getApiVersion() {
+    return apiVersion;
+  }
+
+  @Override
+  @JsonProperty("apiVersion")
+  public void setApiVersion(String apiVersion) {
+    this.apiVersion = apiVersion;
+  }
+
+  @Override
+  @JsonProperty("kind")
+  public String getKind() {
+    return kind;
+  }
+
+  @JsonProperty("kind")
+  public void setKind(String kind) {
+    this.kind = kind;
+  }
+
+  @Override
+  @JsonProperty("metadata")
+  public ObjectMeta getMetadata() {
+    return metadata;
+  }
+
+  @Override
+  @JsonProperty("metadata")
+  public void setMetadata(ObjectMeta metadata) {
+    this.metadata = metadata;
+  }
+
+}
