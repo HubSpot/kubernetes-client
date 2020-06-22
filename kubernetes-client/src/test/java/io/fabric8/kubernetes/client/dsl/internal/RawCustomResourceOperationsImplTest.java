e158718ba56869a2aa20e41552b5c541d53cb0c8
Bryan Beaudreault <bbeaudreault@hubspot.com>
2020-06-22 10:02:19 -0400

    fix

--- kubernetes-client/src/test/java/io/fabric8/kubernetes/client/dsl/internal/RawCustomResourceOperationsImplTest.java
+++ kubernetes-client/src/test/java/io/fabric8/kubernetes/client/dsl/internal/RawCustomResourceOperationsImplTest.java
@@ -43,7 +43,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Protocol;
 import okhttp3.Request;
 import okhttp3.Response;
-import okhttp3.ResponseBody
+import okhttp3.ResponseBody;
 
 public class RawCustomResourceOperationsImplTest {
   private OkHttpClient mockClient;
