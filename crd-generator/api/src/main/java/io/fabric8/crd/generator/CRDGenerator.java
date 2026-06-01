728b33b06b977f230dba821455e2db06167be050
shorstman <shorstman@hubspot.com>
2026-06-01 11:22:58 -0400

    Missing import

--- crd-generator/api/src/main/java/io/fabric8/crd/generator/CRDGenerator.java
+++ crd-generator/api/src/main/java/io/fabric8/crd/generator/CRDGenerator.java
@@ -46,6 +46,7 @@ import java.util.Set;
 import java.util.stream.Stream;
 
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_EMPTY;
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
 public class CRDGenerator {
 
